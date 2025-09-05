acbadddb83f305348500634082210d2a2107f090
mahdi <ip.mahdi99@gmail.com>
2025-09-05 04:40:37 +0330

    make about section by render
    + making about section by render instead of hard code
    + adding language section

--- website/views.py
+++ website/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_view(request):
-    return render(request, 'website/index.html')
-
+    context = {
+        'Birthday': '8 April 1990',
+        'Website': 'www.example.com',
+        'Phone': '+98 913 123 1234',
+        'City': 'Esfahan, Iran',
+        'Age': 26,
+        'Degree': 'B.Sc. in Mechanical Engineering',
+        'Email': 'example@gmail.com',
+        'Freelance': 'Available',
+    }
+    return render(request, 'website/index.html', context)
