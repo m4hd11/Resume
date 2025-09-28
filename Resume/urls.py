9d0fd37082a540216a3d7f0c9f8bdc4d5a3e5496
mahdi <ip.mahdi99@gmail.com>
2025-09-29 00:37:52 +0330

    delete apps

--- Resume/urls.py
+++ Resume/urls.py
@@ -20,8 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
