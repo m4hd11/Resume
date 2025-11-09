72c2ef31b550e6a7d04715f1d2aa67e8ff6249b5
mahdi <ip.mahdi99@gmail.com>
2025-11-10 01:37:51 +0330

    update settings

--- Resume/setting/prod.py
+++ Resume/setting/prod.py
@@ -7,9 +7,9 @@ from Resume.settings import *
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mahdiiranpour.ir', 'www.mahdiiranpour.ir']
 
 
 # Application definition
--- Resume/wsgi.py
+++ Resume/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume.setting.prod')
 
 application = get_wsgi_application()
